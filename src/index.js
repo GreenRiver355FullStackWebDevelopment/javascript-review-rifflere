6674eb1745aad6f7c8371a319ce89bd9d8d6e899
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-04-06 11:21:33 -0700

    Implemented renderReviews function in onload in index.js to display one static mockup review on page load

--- src/index.js
+++ src/index.js
@@ -23,3 +23,41 @@ const reviews = [
 /////////////////////////////////////////////////////////////////////
 
 //Your Code Below Here////
+
+window.onload = () => {
+  console.log("LOADED!")
+
+  renderReviews();
+}
+
+function renderReviews() {
+  const reviewSection = document.querySelector(".reviews");
+
+  const h1 = document.createElement("h1");
+
+  reviewSection.appendChild(h1);
+
+  const div = document.createElement("div");
+  const image = document.createElement("img");
+  const innerDiv = document.createElement("div");
+  const username = document.createElement("p");
+  const rating = document.createElement("p");
+  const review = document.createElement("p");
+
+  div.className = "review_container";
+  image.src = "./images/avatar2.png";
+  username.textContent = "USERNAME";
+  rating.textContent = "RATING";
+  review.textContent = "REVIEW";
+
+  innerDiv.appendChild(username);
+  innerDiv.appendChild(rating);
+  innerDiv.appendChild(review);
+
+  div.appendChild(image);
+  div.appendChild(innerDiv);
+  
+  reviewSection.appendChild(div);
+}
+
+
